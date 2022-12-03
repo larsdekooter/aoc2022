9eeebf1852270987914803edb95cc66912c537b5
Lars de Kooter <de.kooter.lars@gmail.com>
2022-12-03 10:07:02 +0100

    day 2 part 2

--- day2/index.js
+++ day2/index.js
@@ -8,6 +8,9 @@ const lines = readFileSync("./input.txt")
 const winningCombos = { X: "Y", Y: "Z", Z: "X" };
 const choiseRatings = { X: 1, Y: 2, Z: 3 };
 const choiseMapping = { A: "X", B: "Y", C: "Z" };
+
+// Part 1
+
 // Get the score
 function score([e, p]) {
   const en = choiseMapping[e];
@@ -17,9 +20,36 @@ function score([e, p]) {
 }
 // Log the score
 console.log(
-  lines.reduce(
+  `[Part 1]: ${lines.reduce(
     (acc, [enemy, player]) =>
       score([enemy, player]) + choiseRatings[player] + acc,
     0
-  )
+  )}`
 );
+
+// Part 2
+/**
+ * X = Loss;
+ * Y = Draw
+ * Z = win
+ */
+
+function decission([e, p]) {
+  const en = choiseMapping[e];
+  // Win
+  if (p === "Z") return [winningCombos[en], 6];
+  // loss
+  if (p === "X") {
+    if (en === "X") return ["Z", 0];
+    if (en === "Y") return ["X", 0];
+    return ["Y", 0];
+  }
+  return [en, 3];
+}
+
+const partTwoScore = lines.reduce((acc, [enemy, player]) => {
+  const [response, score] = decission([enemy, player]);
+  return acc + score + choiseRatings[response];
+}, 0);
+
+console.log(`[Part 2]: ${partTwoScore}`);
